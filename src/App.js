652e43cbefc9219c4f2f9aee4fdea433ca7339e2
pratiksj <pratiksharai728@gmail.com>
2022-12-25 14:11:35 +0545

    feat: simple counter application using  useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const App = (props) => {
+  const [counter, setCounter] = useState(0);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+      <button onClick={() => setCounter(counter - 1)}>minus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
     </div>
   );
-}
-
+};
 export default App;
